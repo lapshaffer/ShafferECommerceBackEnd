41f484d2ca5d060393e5f60ec4c1ffffdd1a5e1e
lapshaffer <lapshaffer@gmail.com>
2021-08-31 18:08:46 -0400

    Added foreign keys to remaining association models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,8 +5,6 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 // const { DELETE } = require('sequelize/types/lib/query-types');
 
-// * `Product` belongs to many `Tag` models, and `Tag` belongs to many `Product` models. Allow products to have multiple tags and tags to have many products by using the `ProductTag` through model.
-
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
@@ -21,17 +19,19 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
-    model: ProductTag
+    model: ProductTag,
+    // Need a way to id a product within the junction table
+    foreignKey: 'product_id'
   },
-  // ???
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    // Need a way to id a tag within the junction table
+    foreignKey: 'tag_id'
   },
-  // ???
 });
 
 
